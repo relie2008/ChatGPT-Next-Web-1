f12dc4f4b3d2ad9a4ff78cc5f818fafb7dafa4d0
xiongge <752460320@qq.com>
2023-07-06 16:38:28 +0800

    修改标题名称

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,7 +117,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          ChatGPT-4
         </div>
         <div className={styles["sidebar-sub-title"]}>
           有问题联系客服：share15888
